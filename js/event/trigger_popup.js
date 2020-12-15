fbb75cbab81c4168d15652697c065330e86cb670
hoosawasa <51298767+hoosawasa@users.noreply.github.com>
2020-12-15 00:14:48 -0700

    Edited JS and CSS

--- js/event/trigger_popup.js
+++ js/event/trigger_popup.js
@@ -1,7 +1,5 @@
 var trigger = document.getElementsByClassName("trigger")[0];
 
-var btn = document.getElementById("triggerButton");
-
 window.onclick = function(event) {
 	if (event.target == trigger) {
 		trigger.style.display = "none";
