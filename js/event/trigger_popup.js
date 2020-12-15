da790977b6bf9c3a55353730a5d72f8c7e2908e8
hoosawasa <51298767+hoosawasa@users.noreply.github.com>
2020-12-15 00:08:03 -0700

    Created Test Site

--- js/event/trigger_popup.js
+++ js/event/trigger_popup.js
@@ -2,10 +2,6 @@ var trigger = document.getElementsByClassName("trigger")[0];
 
 var btn = document.getElementById("triggerButton");
 
-btn.onclick = function() {
-	trigger.style.display = "block";
-}
-
 window.onclick = function(event) {
 	if (event.target == trigger) {
 		trigger.style.display = "none";
